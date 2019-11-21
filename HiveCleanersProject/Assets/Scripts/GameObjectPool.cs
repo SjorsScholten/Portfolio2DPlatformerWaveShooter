5f1021325e533ef88f3fc8df2371eac45adb680a
Sjors Scholten <scholtensjors22@gmail.com>
2019-11-21 15:37:52 +0100

    added spawn component

--- HiveCleanersProject/Assets/Scripts/GameObjectPool.cs
+++ HiveCleanersProject/Assets/Scripts/GameObjectPool.cs
@@ -7,16 +7,17 @@ namespace Util {
             where TObject : Component  {
         [SerializeField] private int startAmount = 0;
         [SerializeField] private TObject prefab;
+        
         private readonly Queue<TObject> _objects = new Queue<TObject>();
 
         protected virtual void Awake() => AddObject(startAmount);
 
         public TObject Get() {
-            if (_objects.Count == 0) AddObject(1);
+            if (_objects.Count == 0) AddObject();
             return _objects.Dequeue();
         }
 
-        public void AddObject(int count) {
+        public void AddObject(int count = 1) {
             for (var x = 0; x < count; x++) {
                 var newObject = Instantiate(prefab, this.transform);
                 newObject.gameObject.SetActive(false);
